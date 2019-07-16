05f8fd3fa46a8f64029de0bbfdf884ed407011fa
Unknown <maciej.j.szuba@gmail.com>
2019-07-17 00:38:29 +0200

    mongoose connected to app

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
 
 app.get('/', (req, res) => 
     res.json({msg: 'Bogusia'}));
+
+//connectDB
+connectDB();
 
 //Define routes
 app.use('/api/users', require('./routes/users'));
